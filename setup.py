36e7ab9820a0f1be9fd69298e208fe9cdd85fb7e
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2022-01-22 13:15:37 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+class testRequest:
+  def __init__(self):
+    self.text = 'Thinking'
+
+def getAnswer(token, message, userid, username):
+  try:
+    x = testRequest()
+    while x.text == 'Thinking':
+      x = requests.get('https://api.daniton999.ml/chatbot', json = {'token': token, 'id': userid, 'language': 'de', 'name': username, 'message': message}, stream = True)
+      time.sleep(3)
+    x = x.json()['message']
+  except Exception as es:
+    print(es)
+    x = '_ _'
+  return x
