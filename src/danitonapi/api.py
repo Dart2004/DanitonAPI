70262a3b497556b10f113a151d461adcabbb5321
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2022-01-24 10:15:44 +0100

    Update api.py

--- src/danitonapi/api.py
+++ src/danitonapi/api.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import asyncio
 
 token = ''
 
@@ -24,6 +25,22 @@ class ChatBot:
       while x.text == 'Thinking':
         x = requests.get('https://api.daniton999.ml/chatbot', json = {'token': token, 'id': self.id, 'language': self.language, 'name': self.name, 'message': message}, stream = True)
         time.sleep(3)
+      x = x.json()['message']
+    except Exception as es:
+      print(es)
+      x = ''
+    self.thinking = False
+    return x
+    #reset: reset the chatbot
+  async def getAsyncAnswer(self, message):
+    if self.thinking:
+      return False
+    try:
+      x = testRequest()
+      self.thinking = True
+      while x.text == 'Thinking':
+        x = requests.get('https://api.daniton999.ml/chatbot', json = {'token': token, 'id': self.id, 'language': self.language, 'name': self.name, 'message': message}, stream = True)
+        await asyncio.sleep(3)
       x = x.json()['message']
     except Exception as es:
       print(es)
